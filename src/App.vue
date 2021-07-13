9df10b4acf4e86eb55d44e78943b88ba4177b367
Dmitrii <palgunovdv@gmail.com>
2021-07-13 11:45:31 +0300

    создание формы

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      
+      <Form/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Form from './components/Form';
 export default {
   name: 'App',
 
   components: {
- 
+    Form,
   },
 
   data: () => ({
---
+++ src/components/Form.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <v-alert
+            type="success"
+            v-if="sent"
+        >
+            Заявка отправлена
+        </v-alert>
+        <form
+            v-if="!sent"
+        >
+            <v-text-field
+                v-model="form.amount"
+                type="number"
+                required
+                placeholder="Сумма, руб"
+            />
+            <v-select
+                v-model="form.select"
+                :items="items"
+                label="Валюта для конвертации"
+            />
+            <v-alert
+                border="right"
+                color="blue-grey"
+                dark
+            >
+                Сумма в валюте
+            </v-alert>
+            <v-text-field
+                v-model="email"
+                type="text"
+                required
+                placeholder="email"
+                label="E-mail"
+            />
+            <v-btn
+                type="submit"
+            >
+                
+                Записаться на обмен валюты
+            </v-btn>
+        </form>
+    </div>
+</template>
+<script>
+
+
+export default ({
+    data() {
+        return{
+            sent:false,
+            items:[
+                1,
+                2,
+                3,
+            ],
+            email:"",
+            form:{
+                amount:'',
+                select:"null",
+                currencyAmount: "",
+                email: '',
+            }
+        }
+        
+    },
+})
+</script>
