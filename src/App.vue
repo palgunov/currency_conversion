bad13a72451d209137f9a30862a6de40a30c911b
Dmitrii <palgunovdv@gmail.com>
2021-07-13 11:13:12 +0300

    установка vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <div id="app">
-  </div>
+  <v-app>
+    <v-main>
+      
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
+
   components: {
+ 
+  },
 
-  }
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
